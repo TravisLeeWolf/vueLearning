4f6f6dbe947d7ac65f037ae6f7ac8c8a7544349d
TravisLeeWolf <42667524+TravisLeeWolf@users.noreply.github.com>
2024-06-20 19:05:35 +0900

    add rotating logo

--- web_view/src/components/MainHeader.vue
+++ web_view/src/components/MainHeader.vue
@@ -5,7 +5,7 @@
     class="flex items-center justify-between p-2 shadow-lg bg-blue-400 text-white"
   >
     <a class="flex items-center space-x-2" href="https://travisthecoder.com">
-      <img class="h-10 w-auto" src="/icon.png" alt="Badge Round" />
+      <img class="h-10 w-auto main-icon" src="/icon.png" alt="Badge Round" />
     </a>
     <div class="text-2xl">Vue Projects</div>
     <div class="space-x-2">
@@ -22,3 +22,20 @@
     </div>
   </nav>
 </template>
+
+<style scoped>
+@keyframes logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+}
+  
+@media (prefers-reduced-motion: no-preference) {
+    .main-icon {
+        animation: logo-spin infinite 20s linear;
+    }
+}
+</style>
