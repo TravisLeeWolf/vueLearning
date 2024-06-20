a89d3fbcf0ec9bde5332f5114d403de839f35bbc
TravisLeeWolf <42667524+TravisLeeWolf@users.noreply.github.com>
2024-06-20 15:07:10 +0900

    update config working

--- project2_weatherApp/vite.config.js
+++ project2_weatherApp/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: '/weather-app/',
+  base: '/vue/weather-app/',
   plugins: [vue()],
 });
